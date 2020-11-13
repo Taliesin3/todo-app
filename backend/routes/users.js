d9c345692beb30cc9734deb79bfaa0e69a466709
Taliesin3 <60706094+Taliesin3@users.noreply.github.com>
2020-11-13 17:32:51 +0000

    Added user register and login functionality in backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,22 +1,138 @@
 const router = require("express").Router();
-let User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
+const User = require("../models/user.model");
 
-// GET endpoint for url/users/
+// GET all Users
 router.route("/").get((req, res) => {
   User.find()
     .then(users => res.json(users))  // returns json of users
-    .catch(err => res.status(400).json("Error: " + err));
+    .catch(err => res.status(400).json("GET Error: " + err));
 });
 
 // POST endpoint for url/users/add
-router.route("/add").post((req, res) => {  
-  const username = req.body.username;
+router.post("/register", async (req, res) => {  
+  const { email, password, passwordCheck } = req.body;
+  let username = req.body.username;
+  // If no username provided, use email
+  if (!username) username = email;
 
-  const newUser = new User({username});
+  // Validation
+  try{    // TODO: unsure about use of try/catch vs .then .catch
+    if (!email || !password || !passwordCheck) 
+      return res
+        .status(400)
+        .json({msg: "Please ensure all required fields are submitted."});
+    if (password.length < 5)
+      return res
+        .status(400)
+        .json({msg: "The password must be at least 5 characters long."});
+    if (password !== passwordCheck)
+      return res
+        .status(400)
+        .json({msg: "The passwords do not match."});
+    
+    // Check user doesn't already exist
+    User.find({email: email}, (err, prevUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: "Error: Server error."
+        });
+      } else if (prevUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: "Error: Account already exists."
+        });
+      }
+    });
 
-  newUser.save()
-    .then(() => res.json("User added!"))
-    .catch(err => res.status(400).json("Error: " + err));
+    // Hash password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Save new user
+    const newUser = new User({
+      email,
+      password: passwordHash,
+      username
+    });
+    
+    newUser.save()
+      .then((savedUser) => res.json(savedUser))
+      .catch(err => res.status(400).json("POST Error: " + err));
+      
+  } catch (err) {
+    res.status(500).json({error: err.message});
+  }
+});
+
+// Login route
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validate
+    // email and password are provided
+    if (!email || !password) 
+      res.status(400).json({ msg: "Please ensure all required fields are submitted." })
+    
+    // user is registered
+    const user = await User.findOne({email: email});
+    if (!user)
+      return res.status(400).json({ msg: "That account does not exist." });
+    
+    // password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ msg: "Invalid credentials." });
+    
+    // create JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_TOKEN);
+    
+    // respond to login
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Delete logged in user
+router.delete("/delete", auth, async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.user);
+    res.json(deletedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Check if user token is valid
+router.post("/isTokenValid", async (req, res) => {
+  try {
+    // Validate
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+    
+    const verifiedUser = jwt.verify(token, process.env.JWT_TOKEN);
+    if (!verifiedUser) return res.json(false);
+    
+    const user = User.findById(verifiedUser.id);
+    if (!user) return res.json(false);
+    
+    return res.json(true);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;  
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,11 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
 require("dotenv").config();   // allows us to use a .env file
 
 // Set up server
 const app = express();        // creates express server
-const port = process.env.PORT || 5000;  // assign server to port
+const PORT = process.env.PORT || 5000;  // assign server to port
 
 // Middleware
 app.use(cors());        // applies cors middleware
@@ -31,6 +30,4 @@ app.use("/users", usersRouter);  // visiting url/users will show usersRouter
 app.use("/notes", notesRouter);  // visiting url/notes will show notesRouter
 
 // Start server
-app.listen(port, () => {  // start server
-  console.log(`Server is running on port: ${port}`);
-});
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
