ae6ce42e17518004ec80effa3f91998154ef7d2d
Taliesin3 <60706094+Taliesin3@users.noreply.github.com>
2020-11-21 16:06:43 +0000

    Setting up heroku configurations

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node src/index.js
+web: node acbkend/server.js
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();   // allows us to use a .env file
 
+// need proxy front-backend requests for Heroku
+const path = require("path");   
+
 // Set up server
 const app = express();        // creates express server
 const PORT = process.env.PORT || 5000;  // assign server to port
@@ -28,6 +31,14 @@ const notesRoute = require("./routes/notesRoute");
 
 app.use("/user", userRoute);  // visiting url/users will show usersRouter
 app.use("/notes", notesRoute);  // visiting url/notes will show notesRouter
+
+// proxy front-backend requests for Heroku
+// Serve static files from the React frontend
+app.use(express.static(path.join(__dirname, '../src')));
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../src/public/index.html'));
+});
 
 // Start server
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
