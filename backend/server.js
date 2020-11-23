e1e287634e257e01c1224084c115f2ec1a67d12c
Taliesin3 <60706094+Taliesin3@users.noreply.github.com>
2020-11-23 09:58:44 +0000

    Bugfix - css not served properly

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,17 @@ const notesRoute = require("./routes/notesRoute");
 app.use("/api/user", userRoute);  // visiting url/users will show usersRouter
 app.use("/api/notes", notesRoute);  // visiting url/notes will show notesRouter
 
-if (process.env.NODE_ENV === "production") {
-  // proxy front-backend requests for Heroku
+// Depending on development/production build, send static files from approp place
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   // Serve static files from the React frontend
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/public')));
   // Anything that doesn't match the above, send back index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/public/index.html'));
+  })
+} else {
+  // Same as above, with build pathname instead due to production build
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
   });
