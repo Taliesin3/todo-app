a0c66e632cb1fee8c913c127bcfeb078a26a2536
nathanhmiles <60706094+nathanhmiles@users.noreply.github.com>
2021-04-26 12:33:12 +0100

    Removed body margin

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,41 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import {AppBar, Toolbar, Typography } from "@material-ui/core";
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import AuthOptions from "../auth/AuthOptions";
 
-const useStyles = makeStyles(theme => {
-  return (
-    {
-      root: {
-        flexGrow: 1,
-      },
-      header: {
-        backgroundColor: "primary",
-        padding: theme.spacing(2),
-        color: "#fff",
-        marginBottom: "16px",
-      },
-      title: {
-        fontFamily: "'McLaren', cursive",
-        fontWeight: 200,
-        flexGrow: 1,
-      },
-      headerLink: {
-        textDecoration: "none",
-        color: "inherit",
-      },
-    }
-  )
-})
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      flexGrow: 1,
+    },
+    header: {
+      backgroundColor: "primary",
+      padding: theme.spacing(2),
+      color: "#fff",
+      marginBottom: "16px",
+    },
+    title: {
+      fontFamily: "'McLaren', cursive",
+      fontWeight: 200,
+      flexGrow: 1,
+    },
+    headerLink: {
+      textDecoration: "none",
+      color: "inherit",
+    },
+  };
+});
 
 function Header() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" square className={classes.header}>
+      <AppBar position="static" square fluid={true} className={classes.header}>
         <Toolbar>
           <Typography variant="h4" className={classes.title}>
             <Link className={classes.headerLink} to="/notes">
@@ -47,7 +45,7 @@ function Header() {
         </Toolbar>
       </AppBar>
     </div>
-  )
+  );
 }
 
 export default Header;
