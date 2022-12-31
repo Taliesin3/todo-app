163e0fc7411450a0b22068ed73d56b15ab1f0748
Nathan H Miles <60706094+nathanhmiles@users.noreply.github.com>
2022-12-31 12:43:01 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,23 +22,30 @@ app.use('/api/user', userRoute);
 app.use('/api/notes', notesRoute);
 app.use('/api/lists', listsRoute);
 
-// Start server
 let port;
-console.log('❇️ NODE_ENV is', process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
-    port = process.env.PORT || 3000;
-    app.use(express.static(path.join(__dirname, '../build')));
-    app.get('*', (request, response) => {
-        response.sendFile(path.join(__dirname, '../build', 'index.html'));
-    });
-} else {
+// Depending on development/production build, send static files from approp place
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     port = 3001;
     console.log('⚠️ Not seeing your changes as you develop?');
     console.log(
         "⚠️ Do you need to set 'start': 'npm run development' in package.json?"
     );
+    // Serve static files from the React frontend
+    app.use(express.static(path.join(__dirname, '../public')));
+    // Anything that doesn't match the above, send back index.html
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/../public/index.html'));
+    });
+} else {
+    port = process.env.PORT || 3000;
+    // Same as above, with build pathname instead due to production build
+    app.use(express.static(path.join(__dirname, '../build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/../build/index.html'));
+    });
 }
 
+// Start server
 const listener = app.listen(port, () => {
     console.log(
         '❇️ Express server is running on port',
